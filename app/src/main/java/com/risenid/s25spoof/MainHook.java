1e97ad80d06a5d66e7428e4265eb6c74dfb6d514
denny4_user <ya.den.st@mail.ru>
2025-11-16 20:15:58 +0300

    massive update

--- app/src/main/java/com/risenid/s25spoof/MainHook.java
+++ app/src/main/java/com/risenid/s25spoof/MainHook.java
@@ -15,15 +15,15 @@
  * (at your option) any later version.
  */
 
-package com.risenid.huskyspoof;
+package com.risenid.s25spoof;
 
 import android.os.Build;
-import android.os.SystemProperties;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
 
 public class MainHook implements IXposedHookLoadPackage {
 
@@ -31,96 +31,97 @@ public class MainHook implements IXposedHookLoadPackage {
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {        
         XposedBridge.log("GalaxyS25Spoof: Hooking into: " + lpparam.packageName);
 
-        // Hook SystemProperties.get() method - THIS IS THE KEY FOR PUBG MOBILE & AIDA64
-        XposedHelpers.findAndHookMethod(SystemProperties.class, "get", String.class, String.class,
-                new XposedHelpers.MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        String propName = (String) param.args[0];
-                        String defaultValue = (String) param.args[1];
+        try {
+            // Hook SystemProperties.get() via reflection - NO DIRECT IMPORT NEEDED
+            Class<?> systemPropertiesClass = XposedHelpers.findClass("android.os.SystemProperties", null);
+            
+            XposedHelpers.findAndHookMethod(systemPropertiesClass, "get", String.class, String.class,
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            String propName = (String) param.args[0];
 
-                        // ============ DEVICE PROPERTIES ============
-                        if (propName.equals("ro.product.manufacturer")) {
-                            param.setResult("samsung");
-                        } else if (propName.equals("ro.product.brand")) {
-                            param.setResult("samsung");
-                        } else if (propName.equals("ro.product.device")) {
-                            param.setResult("pa3q");
-                        } else if (propName.equals("ro.product.model")) {
-                            param.setResult("SM-S938N");
-                        } else if (propName.equals("ro.product.name")) {
-                            param.setResult("pa3q");
-
-                        // ============ PROCESSOR & CHIPSET ============
-                        } else if (propName.equals("ro.soc.model")) {
-                            param.setResult("SM8750");
-                        } else if (propName.equals("ro.hardware")) {
-                            param.setResult("qcom");
-                        } else if (propName.equals("ro.board.platform")) {
-                            param.setResult("msmnile");
-
-                        // ============ GPU & GRAPHICS (CRITICAL FOR PUBG MOBILE & AIDA64) ============
-                        } else if (propName.equals("ro.hardware.egl")) {
-                            param.setResult("adreno");
-                        } else if (propName.equals("ro.hardware.gpu")) {
-                            param.setResult("adreno830");
-                        } else if (propName.equals("ro.board.gpu")) {
-                            param.setResult("Adreno 830");
-                        } else if (propName.equals("ro.opengles.version")) {
-                            param.setResult("196610"); // OpenGL ES 3.2
-                        } else if (propName.equals("ro.gpu.renderer")) {
-                            param.setResult("Adreno 830");
-                        } else if (propName.equals("ro.gpu.version")) {
-                            param.setResult("1100");
-                        } else if (propName.equals("ro.gpu.api_version")) {
-                            param.setResult("3.2");
-
-                        // ============ MEMORY CONFIGURATION ============
-                        } else if (propName.equals("ro.memory.device")) {
-                            param.setResult("lpddr5x");
-                        } else if (propName.equals("ro.memory.dram")) {
-                            param.setResult("5333");
-                        } else if (propName.equals("ro.storage.impl")) {
-                            param.setResult("ufs40");
-
-                        // ============ DISPLAY ============
-                        } else if (propName.equals("ro.display.lcd_density")) {
-                            param.setResult("498");
-                        } else if (propName.equals("ro.display.resolution")) {
-                            param.setResult("1440x3120");
-                        } else if (propName.equals("ro.display.refresh_rate")) {
-                            param.setResult("120");
-
-                        // ============ ANDROID VERSION ============
-                        } else if (propName.equals("ro.build.version.release")) {
-                            param.setResult("15");
-                        } else if (propName.equals("ro.build.version.sdk")) {
-                            param.setResult("35");
-                        } else if (propName.equals("ro.build.version.security_patch")) {
-                            param.setResult("2025-01-01");
-
-                        // ============ BUILD INFO ============
-                        } else if (propName.equals("ro.build.id")) {
-                            param.setResult("AP38.245");
-                        } else if (propName.equals("ro.build.fingerprint")) {
-                            param.setResult("samsung/pa3q/pa3q:15/AP38.245/S938NKSU1AYF6:user/release-keys");
-                        } else if (propName.equals("ro.build.type")) {
-                            param.setResult("user");
-                        } else if (propName.equals("ro.build.tags")) {
-                            param.setResult("release-keys");
+                            if (propName.equals("ro.product.manufacturer")) {
+                                param.setResult("samsung");
+                            } else if (propName.equals("ro.product.brand")) {
+                                param.setResult("samsung");
+                            } else if (propName.equals("ro.product.device")) {
+                                param.setResult("pa3q");
+                            } else if (propName.equals("ro.product.model")) {
+                                param.setResult("SM-S938N");
+                            } else if (propName.equals("ro.product.name")) {
+                                param.setResult("pa3q");
+                            } else if (propName.equals("ro.soc.model")) {
+                                param.setResult("SM8750");
+                            } else if (propName.equals("ro.hardware")) {
+                                param.setResult("qcom");
+                            } else if (propName.equals("ro.board.platform")) {
+                                param.setResult("msmnile");
+                            } else if (propName.equals("ro.hardware.egl")) {
+                                param.setResult("adreno");
+                            } else if (propName.equals("ro.hardware.gpu")) {
+                                param.setResult("adreno830");
+                            } else if (propName.equals("ro.board.gpu")) {
+                                param.setResult("Adreno 830");
+                            } else if (propName.equals("ro.opengles.version")) {
+                                param.setResult("196610");
+                            } else if (propName.equals("ro.gpu.renderer")) {
+                                param.setResult("Adreno 830");
+                            } else if (propName.equals("ro.gpu.version")) {
+                                param.setResult("1100");
+                            } else if (propName.equals("ro.gpu.api_version")) {
+                                param.setResult("3.2");
+                            } else if (propName.equals("ro.memory.device")) {
+                                param.setResult("lpddr5x");
+                            } else if (propName.equals("ro.memory.dram")) {
+                                param.setResult("5333");
+                            } else if (propName.equals("ro.storage.impl")) {
+                                param.setResult("ufs40");
+                            } else if (propName.equals("ro.display.lcd_density")) {
+                                param.setResult("498");
+                            } else if (propName.equals("ro.display.resolution")) {
+                                param.setResult("1440x3120");
+                            } else if (propName.equals("ro.display.refresh_rate")) {
+                                param.setResult("120");
+                            } else if (propName.equals("ro.build.version.release")) {
+                                param.setResult("15");
+                            } else if (propName.equals("ro.build.version.sdk")) {
+                                param.setResult("35");
+                            } else if (propName.equals("ro.build.version.security_patch")) {
+                                param.setResult("2025-01-01");
+                            } else if (propName.equals("ro.build.id")) {
+                                param.setResult("AP38.245");
+                            } else if (propName.equals("ro.build.fingerprint")) {
+                                param.setResult("samsung/pa3q/pa3q:15/AP38.245/S938NKSU1AYF6:user/release-keys");
+                            } else if (propName.equals("ro.build.type")) {
+                                param.setResult("user");
+                            } else if (propName.equals("ro.build.tags")) {
+                                param.setResult("release-keys");
+                            }
                         }
-                    }
-                });
+                    });
+
+            XposedBridge.log("GalaxyS25Spoof: SystemProperties hook applied ✓");
+
+        } catch (Throwable e) {
+            XposedBridge.log("GalaxyS25Spoof: Error hooking SystemProperties: " + e.getMessage());
+            e.printStackTrace();
+        }
 
         // Also hook Build class for compatibility
-        XposedHelpers.setStaticObjectField(Build.class, "MANUFACTURER", "samsung");
-        XposedHelpers.setStaticObjectField(Build.class, "BRAND", "samsung");
-        XposedHelpers.setStaticObjectField(Build.class, "DEVICE", "pa3q");
-        XposedHelpers.setStaticObjectField(Build.class, "MODEL", "SM-S938N");
-        XposedHelpers.setStaticObjectField(Build.class, "PRODUCT", "pa3q");
-        XposedHelpers.setStaticObjectField(Build.class, "SOC.MODEL", "SM8750");
-        XposedHelpers.setStaticObjectField(Build.class, "FINGERPRINT",
-                "samsung/pa3q/pa3q:15/AP38.245/S938NKSU1AYF6:user/release-keys");
+        try {
+            XposedHelpers.setStaticObjectField(Build.class, "MANUFACTURER", "samsung");
+            XposedHelpers.setStaticObjectField(Build.class, "BRAND", "samsung");
+            XposedHelpers.setStaticObjectField(Build.class, "DEVICE", "pa3q");
+            XposedHelpers.setStaticObjectField(Build.class, "MODEL", "SM-S938N");
+            XposedHelpers.setStaticObjectField(Build.class, "PRODUCT", "pa3q");
+            XposedHelpers.setStaticObjectField(Build.class, "FINGERPRINT",
+                    "samsung/pa3q/pa3q:15/AP38.245/S938NKSU1AYF6:user/release-keys");
+            
+            XposedBridge.log("GalaxyS25Spoof: Build class hook applied ✓");
+        } catch (Throwable e) {
+            XposedBridge.log("GalaxyS25Spoof: Error hooking Build class: " + e.getMessage());
+        }
 
         XposedBridge.log("GalaxyS25Spoof: Device spoofed to Samsung Galaxy S25 Ultra ✓");
         XposedBridge.log("GalaxyS25Spoof: GPU spoofed to Adreno 830 (1100MHz) ✓");
